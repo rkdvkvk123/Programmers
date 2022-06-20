dcd6d5af21d4dfdd54b4a51bbb7fb3a4c59f700e
rkdvk <rkdvk@DESKTOP-9DDV50G>
2022-06-21 00:18:38 +0900

    bebe

---
+++ src/level1/SolutionExample001.java
@@ -0,0 +1,22 @@
+package level1;
+
+class Solution001 {
+	public int[] solution(String[] id_list, String[] report, int k) {
+		int[] answer = new int[id_list.length];
+		
+		
+		return answer;
+	}
+}
+
+public class SolutionExample001 {
+	public static void main(String[] args) {
+		Solution001 solution001 = new Solution001();
+		
+		String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+		String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+		int k = 2;
+		
+	}
+
+}
